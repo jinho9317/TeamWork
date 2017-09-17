ded54d9285ba2c1e5b917576af6207ff8c39f214
mak3216 <32036000+mak3216@users.noreply.github.com>
2017-09-18 00:18:52 +0900

    Add files via upload

---
+++ TeamWork/src/S_Team.java
@@ -0,0 +1,30 @@
+﻿
+public class S_Team {
+
+	S_Team()
+	{
+		System.out.println("Hello World!");
+	}
+	
+	private void TeamLeader()
+	{
+		System.out.println("Leader : 32121875 ±èÇöÀÏ");
+	}
+	private void Engineer(
+		System.out.println("Engineer : 32151501 sohyun);
+	)
+	//private void Reverser()
+	private void Supporter()
+        {
+         System.out.println("Engineer : 32121846 Lee Dong Hyeong);
+        }
+	
+	public static void main(String[] args) {
+		S_Team Team = new S_Team();
+		Team.TeamLeader();
+		//TEam.Engineer();
+		//Team.Reverser();
+		Team.Supporter();		
+	}	
+	
+}
